67ed6ca8a48aea6c5de2df260652b8ac3ff95347
Constantin Pascal <costea32@gmail.com>
2017-07-23 00:19:52 +0300

    issue #3 Adding ElementType RadioButtonGroup
    Adding basic functionality without much error handling, basic
    functionality includes selecting a radio button by value or index, and
    getting the selected value or index

---
+++ junit-tests/src/test/java/org/testmonkeys/sut/demoqa/playground/RadioComponentTest.java
@@ -0,0 +1,31 @@
+package org.testmonkeys.sut.demoqa.playground;
+
+import org.junit.Test;
+import org.testmonkeys.koshmar.core.browser.Browser;
+import org.testmonkeys.koshmar.core.browser.DriverFactory;
+import org.testmonkeys.koshmar.core.factory.PageFactory;
+import org.testmonkeys.koshmar.core.factory.PageScanner;
+import org.testmonkeys.sut.demoqa.HomePage;
+import org.testmonkeys.sut.demoqa.RegistrationPage;
+
+public class RadioComponentTest {
+
+    @Test
+    public void radioBtnComponent(){
+
+        DriverFactory df = new DriverFactory();
+        Browser browser= new Browser(df.initDriver("chrome"));
+        //browser.goTo("http://www.demoqa.com");
+        PageFactory pageFactory=new PageFactory(browser,new PageScanner("org.testmonkeys.sut"),"http://demoqa.com");
+
+        RegistrationPage registrationPage=pageFactory.createPage(RegistrationPage.class);
+        registrationPage.open();
+        registrationPage.getMaritalStatus().selectByIndex(2);
+        registrationPage.getMaritalStatus().selectByValue("married");
+        for (String s:registrationPage.getMaritalStatus().getValues())
+            System.out.println(s);
+        System.out.println(":"+registrationPage.getMaritalStatus().getSelectedValue());
+        registrationPage.getUrl();
+
+    }
+}
