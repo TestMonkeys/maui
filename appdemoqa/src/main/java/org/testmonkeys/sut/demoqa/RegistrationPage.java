d076526e583918f62c1556235f8dce242baedf58
monofrei <onofrei.marin@gmail.com>
2017-08-07 19:56:19 +0300

    fixes #5 implement checkbox html element (#19)

--- appdemoqa/src/main/java/org/testmonkeys/sut/demoqa/RegistrationPage.java
+++ appdemoqa/src/main/java/org/testmonkeys/sut/demoqa/RegistrationPage.java
@@ -3,18 +3,25 @@ package org.testmonkeys.sut.demoqa;
 import org.testmonkeys.koshmar.pageobjects.AbstractPage;
 import org.testmonkeys.koshmar.pageobjects.ElementAccessor;
 import org.testmonkeys.koshmar.pageobjects.PageAccessor;
-import org.testmonkeys.koshmar.pageobjects.elements.Button;
+import org.testmonkeys.koshmar.pageobjects.elements.CheckBox;
 import org.testmonkeys.koshmar.pageobjects.elements.GroupComponent;
 import org.testmonkeys.koshmar.pageobjects.elements.RadioButtonGroup;
 
-@PageAccessor(name = "Registration Page", url="/registration")
+@PageAccessor(name = "Registration Page", url = "")
 public class RegistrationPage extends AbstractPage {
 
-    @ElementAccessor(elementName="Interactions", byName = "radio_4[]")
+    @ElementAccessor(elementName = "Interactions", byName = "radio_4[]")
     private RadioButtonGroup maritalStatus;
+
+    @ElementAccessor(elementName = "Hobby", byXPath = "//div[@class='radio_wrap']/input[@type='checkbox']")
+    private GroupComponent<CheckBox> hobby;
 
 
     public RadioButtonGroup getMaritalStatus() {
         return maritalStatus;
+    }
+
+    public GroupComponent<CheckBox> hobbyCheckBoxes() {
+        return hobby;
     }
 }
---
+++ junit-tests/src/test/java/org/testmonkeys/sut/demoqa/playground/CheckBoxComponentTest.java
@@ -0,0 +1,73 @@
+package org.testmonkeys.sut.demoqa.playground;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.testmonkeys.koshmar.core.factory.PageFactory;
+import org.testmonkeys.koshmar.core.factory.PageScanner;
+import org.testmonkeys.koshmar.pageobjects.elements.CheckBox;
+import org.testmonkeys.koshmar.pageobjects.elements.GroupComponent;
+import org.testmonkeys.sut.demoqa.RegistrationPage;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CheckBoxComponentTest extends AbstractComponentTest {
+
+    private RegistrationPage registrationPage;
+
+    @Before
+    public void beforeScenario() throws IOException {
+        PageFactory pageFactory = new PageFactory(browser, new PageScanner("org.testmonkeys.sut"),
+                htmlPage.getFile().getAbsolutePath());
+        registrationPage = pageFactory.createPage(RegistrationPage.class);
+        registrationPage.open();
+    }
+
+    @Test
+    public void check() {
+        GroupComponent<CheckBox> checkBoxGroupComponent = registrationPage.hobbyCheckBoxes();
+        List<CheckBox> hobbies = checkBoxGroupComponent.getAll();
+        assertThat(hobbies).hasSize(3);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isFalse());
+        hobbies.forEach(CheckBox::check);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isTrue());
+    }
+
+    @Test
+    public void checkAlreadyChecked() {
+        GroupComponent<CheckBox> checkBoxGroupComponent = registrationPage.hobbyCheckBoxes();
+        List<CheckBox> hobbies = checkBoxGroupComponent.getAll();
+        assertThat(hobbies).hasSize(3);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isFalse());
+        hobbies.forEach(CheckBox::check);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isTrue());
+        hobbies.forEach(CheckBox::check);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isTrue());
+    }
+
+    @Test
+    public void uncheck() {
+        GroupComponent<CheckBox> checkBoxGroupComponent = registrationPage.hobbyCheckBoxes();
+        List<CheckBox> hobbies = checkBoxGroupComponent.getAll();
+        assertThat(hobbies).hasSize(3);
+        hobbies.forEach(CheckBox::check);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isTrue());
+        hobbies.forEach(CheckBox::uncheck);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isFalse());
+    }
+
+    @Test
+    public void uncheckAlreadyUnchecked() {
+        GroupComponent<CheckBox> checkBoxGroupComponent = registrationPage.hobbyCheckBoxes();
+        List<CheckBox> hobbies = checkBoxGroupComponent.getAll();
+        assertThat(hobbies).hasSize(3);
+        hobbies.forEach(CheckBox::check);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isTrue());
+        hobbies.forEach(CheckBox::uncheck);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isFalse());
+        hobbies.forEach(CheckBox::uncheck);
+        hobbies.forEach(h -> assertThat(h.isChecked()).isFalse());
+    }
+}
