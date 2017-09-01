8fa82dc58bd959317bcd0ca85022f99e1739843f
Constantin Pascal <costea32@gmail.com>
2017-09-01 17:28:48 +0300

    Adding docs and error handling

--- koshmar/src/main/java/org/testmonkeys/koshmar/pageobjects/elements/html/HtmlElement.java
+++ koshmar/src/main/java/org/testmonkeys/koshmar/pageobjects/elements/html/HtmlElement.java
@@ -1,64 +1,81 @@
 package org.testmonkeys.koshmar.pageobjects.elements.html;
 
-import org.testmonkeys.koshmar.core.elements.actions.ExecuteJSAction;
 import org.testmonkeys.koshmar.core.elements.actions.ExecuteJSScript;
 import org.testmonkeys.koshmar.pageobjects.elements.AbstractComponent;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Scanner;
 
 /**
  * HtmlElement provides access to functionality available for any Element present in DOM
  */
 public class HtmlElement {
 
+    private final String GET_ATTRIBUTE_SCRIPT = "/javaScript/htmlElement/getAttributes.js";
+    private final String GET_COMPUTED_STYLE_SCRIPT = "/javaScript/htmlElement/getComputedStyle.js";
     private AbstractComponent component;
 
-    public HtmlElement(AbstractComponent component){
+    public HtmlElement(AbstractComponent component) {
         this.component = component;
     }
 
     /**
      * Gets all attributes declared on this DOM element with their values.
      *
-     * @return List of {@link=HtmlAttribute}
+     * @return List of HtmlAttribute
      */
     public List<HtmlAttribute> getAttributes() {
-        Object result = new ExecuteJSScript(component, "/javaScript/htmlElement/getAttributes.js").execute();
+        Object result = new ExecuteJSScript(component, GET_ATTRIBUTE_SCRIPT).execute();
 
         List<HtmlAttribute> attributes = new ArrayList<>();
         try {
             ArrayList<Map<String, String>> elementAttributes = (ArrayList<Map<String, String>>) result;
             for (Map<String, String> elementAttribute : elementAttributes) {
-                HtmlAttribute attribute = new HtmlAttribute();
-                if (elementAttribute.containsKey("name")) {
-                    attribute.setName(elementAttribute.get("name"));
-                }
-                if (elementAttribute.containsKey("value")) {
-                    attribute.setValue(elementAttribute.get("value"));
-                }
-                attributes.add(attribute);
+                attributes.add(getAttribute(elementAttribute));
             }
         } catch (Exception e) {
-            //TODO: log error
+            throw new JSInteractionException("Could not parse annotations", e);
         }
 
         return attributes;
     }
 
-    public Map<String, String> getStyle() {
-        Map<String, String> result = null;
+    /**
+     * Parses the HashMap of each attribute and builds an the HtmlAttribute model
+     *
+     * @param attributeDetails hash map of attribute properties
+     * @return HtmlAttribute
+     */
+    private HtmlAttribute getAttribute(Map<String, String> attributeDetails) {
+        HtmlAttribute attribute = new HtmlAttribute();
+        if (attributeDetails.containsKey("name")) {
+            attribute.setName(attributeDetails.get("name"));
+        } else {
+            throw new JSInteractionException("Could not understand the attribute list");
+        }
 
-        Object jsResult = new ExecuteJSScript(component, "/javaScript/htmlElement/getComputedStyle.js"
-        ).execute();
+        if (attributeDetails.containsKey("value")) {
+            attribute.setValue(attributeDetails.get("value"));
+        }
+
+        return attribute;
+    }
+
+    /**
+     * Gets the computed style of the element. This will include both the style declared on the element
+     * as well as any generic style that was applied to it.
+     *
+     * @return Map of style-name to style-value pairs
+     */
+    public Map<String, String> getStyle() {
+        Object jsResult = new ExecuteJSScript(component, GET_COMPUTED_STYLE_SCRIPT).execute();
+
+        Map<String, String> result;
         try {
             result = (Map<String, String>) jsResult;
         } catch (ClassCastException e) {
-            //todo: log and throw new exception
+            throw new JSInteractionException("Could not parse style object", e);
         }
         return result;
     }
