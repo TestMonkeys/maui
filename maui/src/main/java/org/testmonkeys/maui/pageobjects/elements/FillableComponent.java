be6c77eb2190de910a86e7c0aec5199f875e3e06
Constantin Pascal <costea32@gmail.com>
2018-06-30 17:06:37 +0300

    Rebranding to maui

---
+++ maui/src/main/java/org/testmonkeys/maui/pageobjects/elements/FillableComponent.java
@@ -0,0 +1,15 @@
+package org.testmonkeys.maui.pageobjects.elements;
+
+/**
+ * Interface to define components that can be filled with data, such as
+ * inputs, selects, checkboxes etc.
+ */
+public interface FillableComponent<T> {
+
+    /**
+     * Fills the component with data.
+     *
+     * @param data - data to fill in
+     */
+    void fill(T data);
+}
