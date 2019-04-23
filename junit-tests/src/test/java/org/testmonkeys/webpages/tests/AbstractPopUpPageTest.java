fb91ec35cd6c83d77cc20f8ffefa85cfa777bc42
Constantin Pascal <costea32@gmail.com>
2019-04-23 15:05:10 +0300

    Integrating jetty plugin

--- junit-tests/src/test/java/org/testmonkeys/webpages/tests/AbstractPopUpPageTest.java
+++ junit-tests/src/test/java/org/testmonkeys/webpages/tests/AbstractPopUpPageTest.java
@@ -22,7 +22,7 @@ public class AbstractPopUpPageTest extends AbstractComponentTest {
     @Before
     public void beforeScenario() throws IOException {
         pageFactory = new PageFactory(browser, new PageScanner("org.testmonkeys.webpages"),
-                "http://localhost:8080/JSPopUpPage.html");
+                "http://localhost:8080/testpages/JSPopUpPage.html");
 //                "file:///" + htmlPage.getFile().getAbsolutePath());
         popUpPage = pageFactory.createPage(JsPopUpPage.class);
         popUpPage.open();
