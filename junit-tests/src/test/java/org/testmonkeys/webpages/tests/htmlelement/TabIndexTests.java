14392fe7a13993042844b7a9d7dac1c8247f0171
Constantin Pascal <costea32@gmail.com>
2019-04-28 18:09:17 +0300

    adjusting tabindex tests as default tabindex is different for EDGE

--- junit-tests/src/test/java/org/testmonkeys/webpages/tests/htmlelement/TabIndexTests.java
+++ junit-tests/src/test/java/org/testmonkeys/webpages/tests/htmlelement/TabIndexTests.java
@@ -1,5 +1,6 @@
 package org.testmonkeys.webpages.tests.htmlelement;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.testmonkeys.webpages.tests.AbstractHtmlElementPageTest;
 
@@ -11,13 +12,13 @@ public class TabIndexTests extends AbstractHtmlElementPageTest {
     @Test
     public void get_tabIndex() {
         int res = page.header().getHtmlElement().getTabIndex();
-        assertThat(res, is(-1));
+        Assert.assertTrue("tab index less or equal to 0", res<=0 );
     }
 
     @Test
     public void set_tabIndex() {
         int res = page.header().getHtmlElement().getTabIndex();
-        assertThat(res, is(-1));
+        Assert.assertTrue("tab index less or equal to 0", res<=0 );
         page.header().getHtmlElement().setTabIndex(1);
 
         res = page.header().getHtmlElement().getTabIndex();
